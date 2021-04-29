bbb72136ddcf3a2e22f9f8dc7da71103162c03e3
Rhishikesh Sapkal <rhishikesh_sapkal@persistent.com>
2021-04-29 09:21:13 +0530

    Added Function Headers

--- demo_Math.py
+++ demo_Math.py
@@ -1,9 +1,13 @@
+#Add implementation
 def add(x,y):
     return x+y
+#Subtract implementation    
 def subtract(x,y):
     return x-y
+#Multiply implementation
 def multiply(x,y):
     return x*y #
+#Divide implementation
 def divide(x,y):
     return x/y # adding conflict master
 
